a7db4c28087549a572db50de2bda32a8f46da0f0
Harshit <itsharshitraj@gmail.com>
2025-03-06 15:53:28 +0530

    [Harshit]Add . UC5-Added validation for user input: Initial caps & min 3 characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   userName: string = '';
+  errorMessage: string = '';
+  showGreeting: boolean = false;
 
-  
+     
+
+  validateName() {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/; 
+
+    if (this.userName.trim().length ===0) {
+      this.errorMessage = ''; 
+      this.showGreeting = false;
+
+    } else if(!namePattern.test(this.userName)) {
+      this.errorMessage = 'Invalid name! Must start with uppercase and have at least 3 letters.';
+      this.showGreeting = false;
+
+    } else {
+      this.errorMessage = ''; 
+      this.showGreeting = true;
+    }
+  }
 }
